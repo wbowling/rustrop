3c840ee638224b9ccc02a36e1b2779d750251da1
William Bowling <will@wbowling.info>
2017-12-21 20:46:04 +1100

    Use a struct

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,45 @@ extern crate elf;
 extern crate capstone;
 
 use std::path::PathBuf;
-use capstone::Capstone;
-use capstone::arch;
-use capstone::arch::BuildsCapstone;
-use capstone::arch::BuildsCapstoneSyntax;
-use capstone::Insn;
-use std::collections::HashMap;
+use capstone::{Capstone, arch, Insn};
+use capstone::arch::{BuildsCapstone, BuildsCapstoneSyntax};
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use std::env;
+
+pub struct Gadget {
+    addr: u64,
+    asm: Vec<Insn>,
+    output: String,
+}
+
+impl Gadget {
+    pub fn new(addr: u64, asm: Vec<Insn>) -> Gadget {
+        let output = asm.iter()
+                            .map({|inst| format!("{} {}\n", inst.mnemonic().unwrap_or(""), inst.op_str().unwrap_or(""))})
+                            .collect();
+        Gadget { addr: addr, asm: asm, output: output }
+    }
+}
+
+impl PartialEq for Gadget {
+    fn eq(&self, other: &Gadget) -> bool {
+        self.output == other.output
+    }
+}
+
+impl Eq for Gadget {}
+
+impl Hash for Gadget {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.output.hash(state);
+    }
+}
 
 fn main() {
-    let path = PathBuf::from("testcases/baby_stack");
+    let arg1 = env::args().nth(1).expect("Must pass in arg");
+
+    let path = PathBuf::from(arg1);
     let file = match elf::File::open_path(&path) {
         Ok(f) => f,
         Err(e) => panic!("Error: {:?}", e),
@@ -20,18 +50,18 @@ fn main() {
         .iter()
         .filter({ |s| (s.shdr.flags.0 & elf::types::SHF_EXECINSTR.0) != 0});
 
-    let mut map = HashMap::new();
+    let mut set = HashSet::new();
     for section in executabe {
-        map = dis(section.data.as_slice(), section.shdr.addr, map);
+        set = dis(section.data.as_slice(), section.shdr.addr, set);
     }
 
-    for entry in map.values() {
-        println!("{}", entry);
+    for entry in set {
+        println!("Found 0x{:x}:\n{}\n", entry.addr, entry.output);
     }
 }
 
 
-fn dis(bytes: &[u8], addr: u64, mut map:HashMap<String, String>) -> HashMap<String, String>{
+fn dis(bytes: &[u8], addr: u64, mut map:HashSet<Gadget>) -> HashSet<Gadget>{
     let cs: Capstone = Capstone::new()
         .x86()
         .mode(arch::x86::ArchMode::Mode64)
@@ -53,14 +83,19 @@ fn dis(bytes: &[u8], addr: u64, mut map:HashMap<String, String>) -> HashMap<Stri
                     Ok(insns) => {
                         match insns.iter().last() {
                             Some(ref inst) if is_ret(inst) => {
-                                let mut val = String::new();
                                 let mut key = String::new();
+                                let mut asm = Vec::new();
                                 for inst in insns.iter() {
-                                    key.push_str(&format!("{} {}", inst.mnemonic().unwrap_or(""), inst.op_str().unwrap_or("")));
-                                    val.push_str(&format!("0x{:x} {} {}\n", inst.address(), inst.mnemonic().unwrap_or(""), inst.op_str().unwrap_or("")));
-                                    if is_ret(&inst) { break }
+                                    key.push_str(&format!("{} {}\n", inst.mnemonic().unwrap_or(""), inst.op_str().unwrap_or("")));
+                                    let done = is_ret(&inst);
+                                    asm.push(inst);
+                                    if done { break }
                                 }
-                                map.insert(key, val);
+                                let addr = asm.first().map({|i| i.address()});
+                                if let Some(a) = addr {
+                                    map.insert(Gadget::new(a, asm));
+                                }
+
                             },
                             _ => {}
                         }
