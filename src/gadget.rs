909d2cb281807e51d788d28a52bb32b6afc5548b
William Bowling <will@wbowling.info>
2017-12-22 22:26:55 +1100

    Faster?

--- src/gadget.rs
+++ src/gadget.rs
@@ -15,17 +15,27 @@ pub struct Gadget {
 impl Gadget {
     pub fn new(addr: u64, asm: Vec<Insn>) -> Gadget {
         let output: Vec<String> = asm.iter()
-            .map({|inst| format!("{} {}", inst.mnemonic().unwrap_or(""), inst.op_str().unwrap_or(""))})
-            .collect();
+            .map(|inst| {
+                let mut s = String::with_capacity(50);
+                s.push_str(inst.mnemonic().unwrap_or(""));
+                s.push_str(" ");
+                s.push_str(inst.op_str().unwrap_or(""));
+                s
+            }).collect();
         Gadget { addr: addr, asm: asm, output: output.join("; ") }
     }
 
     pub fn color(&self) -> String {
         let offset = Cyan.paint(format!("0x{:x}", self.addr));
         let output: Vec<String> = self.asm.iter()
-            .map({|inst| format!("{} {}",
-                                 Red.paint(inst.mnemonic().unwrap_or("")),
-                                 Yellow.paint(inst.op_str().unwrap_or("")))
+            .map(|inst| {
+                let mut s = String::with_capacity(100);
+
+                s.push_str(&Red.paint(inst.mnemonic().unwrap_or("")).to_string());
+                s.push_str(" ");
+                s.push_str(&Yellow.paint(inst.op_str().unwrap_or("")).to_string());
+
+                s
             }).collect();
 
         format!("{}: {}", offset, output.join("; "))
